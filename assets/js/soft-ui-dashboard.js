fff4189626c981c426e53564b24514872107e07c
Reza Esmaili <bitbucket@dfox.info>
2022-11-08 13:34:04 +0100

    Fix JS and sidebar

--- assets/js/soft-ui-dashboard.js
+++ assets/js/soft-ui-dashboard.js
@@ -432,18 +432,23 @@ let referenceButtons = document.querySelector('[data-class]');
 window.addEventListener("resize", navbarColorOnResize);
 
 function navbarColorOnResize() {
+
   if (window.innerWidth > 1200) {
-    if (referenceButtons.classList.contains('active') && referenceButtons.getAttribute('data-class') === 'bg-transparent') {
-      sidenav.classList.remove('bg-white');
-    } else {
-      sidenav.classList.add('bg-white');
+    // if (referenceButtons.classList.contains('active') && referenceButtons.getAttribute('data-class') === 'bg-transparent') {
+    //   sidenav.classList.remove('bg-white');
+    // } else {
+    if (sidenav) {
+      sidenav.classList.remove('bg-white')
+      sidenav.classList.add('bg-transparent')
     }
+    // }
   } else {
-    sidenav.classList.add('bg-white');
-    sidenav.classList.remove('bg-transparent');
+    if (sidenav) {
+      sidenav.classList.add('bg-white')
+      sidenav.classList.remove('bg-transparent')
+    }
   }
 }
-
 // Deactivate sidenav type buttons on resize and small screens
 window.addEventListener("resize", sidenavTypeOnResize);
 window.addEventListener("load", sidenavTypeOnResize);
--- components/admin/Sidebar.vue
+++ components/admin/Sidebar.vue
@@ -1,32 +1,7 @@
 <template>
-  <aside
-    class="
-      sidenav
-      navbar navbar-vertical navbar-expand-xs
-      border-0 border-radius-xl
-      my-3
-      fixed-left
-      ms-3
-    "
-    id="sidenav-main"
-  >
+  <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3" id="sidenav-main">
     <div class="sidenav-header">
-      <i
-        class="
-          fas
-          fa-times
-          p-3
-          cursor-pointer
-          text-secondary
-          opacity-5
-          position-absolute
-          right-0
-          top-0
-          d-none d-xl-none
-        "
-        aria-hidden="true"
-        id="iconSidenav"
-      ></i>
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <router-link :to="{ name: 'admin.dashboard' }" class="navbar-brand m-0">
         <img
           src="@/assets/img/pm-logo-small.png"

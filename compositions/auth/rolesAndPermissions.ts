b7c0be21bf0a0936584c9787b7aadb2b2200da40
Lukas Roth <mail@lukasroth.net>
2022-11-16 17:16:30 +0100

    added roles and permissions

---
+++ compositions/auth/rolesAndPermissions.ts
@@ -0,0 +1,29 @@
+import { computed, ref, watch } from 'vue'
+import usePermission from './permission'
+import useRole, { Role } from './role'
+import { useUserStore } from '../../store/user'
+import { storeToRefs } from 'pinia'
+
+export default function useRolesAndPermissions() {
+  const userStore = useUserStore()
+  const { user } = storeToRefs(userStore)
+
+  const userPermissions = computed(() => {
+    if (!user.value) {
+      return []
+    }
+    return user.value.permissions
+  })
+
+  const userRoles = computed(() => {
+    if (!user.value) {
+      return []
+    }
+    return user.value.roles
+  })
+
+  return {
+    ...usePermission(userPermissions, userRoles),
+    ...useRole(userRoles),
+  }
+}

2e32da311cfe7234733d83f732d8afe217efdb3d
Lukas Roth <mail@lukasroth.net>
2023-04-11 12:12:19 +0200

    added tagField

---
+++ components/forms/TagField.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="input-group mb-3">
+    <span id="tagError" v-if="data.error">{{ data.error }}</span>
+    <input class="form-control" placeholder="type to add taggings ..." v-model="data.addTag">
+    <button class="btn btn-outline-primary mb-0" id="button-addon2" @click.prevent="pushTag" icon="plus">Tag hinzufügen</button>
+  </div>
+  <div id="tag" v-for="tag in taggings">
+    {{ tag }} <span style="cursor:pointer;" @click="deleteTag(tag)">x</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {onMounted, reactive} from "vue";
+
+const props = defineProps({
+  placeholder: String,
+  data: Array<String>
+});
+
+const data = reactive({
+  addTag: '',
+  error: false
+});
+
+const taggings = ref<Array<String>>([]);
+
+console.log("was geht ab", data)
+
+const emit = defineEmits(['update:modelValue']);
+
+const deleteTag = (tag) => {
+  const index = taggings.value.indexOf(tag);
+  taggings.value.splice(index, 1);
+  emit("update:modelValue", taggings.value);
+};
+
+const pushTag = () => {
+  if (data.addTag.length > 2) {
+    console.log("data",taggings)
+    taggings.value.push(data.addTag);
+    data.addTag = '';
+    emit("update:modelValue", taggings.value);
+    data.error = false;
+  } else {
+    data.error = "Die Eingabe muss 3 zeichen lang sein";
+  }
+};
+
+onMounted(() => {
+  if (props.data) {
+    taggings.value = props.data;
+  }
+});
+
+</script>
+<style scoped lang="scss">
+#tag {
+  margin: 15px 5px 0px 0px;
+  padding: 2px 5px 2px 5px;
+  float: left;
+  background-color: #fda238;
+  border-radius: 10px;
+}
+
+#tag span {
+  cursor: pointer;
+}
+
+#tagError {
+  color: #b41414;
+  font-size: 0.9rem;
+}
+</style>

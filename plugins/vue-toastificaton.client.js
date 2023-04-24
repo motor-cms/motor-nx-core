6b059955eb10cfc590ae3713dece6854e2e552e3
Lukas Roth <mail@lukasroth.net>
2023-04-24 10:55:56 +0200

    rename plugin

--- plugins/vue-toastificaton.client.js
+++
@@ -1,7 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import Toast from 'vue-toastification'
-import 'vue-toastification/dist/index.css' // if needed
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Toast)
-})

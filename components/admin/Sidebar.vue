f76b3170b286527bd179685e80bcfa165f2902a2
Lukas Roth <mail@lukasroth.net>
2023-05-05 11:35:55 +0200

    changed sidebar and login for logo

--- components/admin/Sidebar.vue
+++ components/admin/Sidebar.vue
@@ -2,14 +2,13 @@
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <NuxtLink to="/admin/dashboard" class="navbar-brand m-0">
+      <NuxtLink to="/admin/dashboard" class="d-flex navbar-brand m-0">
         <img
-          src="/img/pm-logo-small.png"
-          style="max-width: 40px"
+          src="~/assets/images/logo.png"
           class="navbar-brand-img"
-          alt="Motor NX"
+          :alt="$t('global.logo.alt')"
         />
-        <span class="ms-1 font-weight-bold">{{ $t('global.project') }}</span>
+        <span v-if="showProjectName" class="ms-1 font-weight-bold">{{ $t('global.project') }}</span>
       </NuxtLink>
     </div>
     <hr class="horizontal dark mt-0" />
@@ -131,6 +130,8 @@ export default defineComponent({
     Skeletor
   },
   setup() {
+    const runtimeConfig = useRuntimeConfig();
+    const showProjectName = computed(() => runtimeConfig.public.showProjectName);
     const navigationStore = useNavigationStore();
     const { activeParent, activeChild , navigationItems, loading} = storeToRefs(navigationStore);
     const { setActiveChild, toggleMenu, getNavigationItems } = navigationStore;
@@ -185,7 +186,8 @@ export default defineComponent({
       activeChild,
       setActiveChild,
       routeParser,
-      loading
+      loading,
+      showProjectName
     }
   },
 })

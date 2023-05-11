f2b6c4da393d92e25d291b915d0f8518d1cb9d4f
Lukas Roth <mail@lukasroth.net>
2023-05-11 14:56:38 +0200

    aded new component Statusicon

---
+++ components/admin/cell/StatusIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <template v-if="record?.[prop]">
+    <fa icon="check" class="text-success"/>
+  </template>
+  <template v-else>
+    <fa icon="xmark"/>
+  </template>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  options: Object,
+  record: Object,
+  prop: String,
+});
+</script>

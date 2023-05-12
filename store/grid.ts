6722dacb5f0672890da5fcbfe7d2a2ba3de15d2e
Lukas Roth <mail@lukasroth.net>
2023-05-12 12:42:34 +0200

    new grid logic for actions and selectable items

--- store/grid.ts
+++ store/grid.ts
@@ -3,11 +3,16 @@ import {reactive, ref} from 'vue'
 
 export const useGridStore = defineStore('grid', () => {
 
+  const meta = ref<Object>();
+  const init = (m: Object) => {
+    meta.value = m;
+  }
+
   const selectedItems = ref<Array<Record<string, any>>>([]);
   const allSelected = ref(false);
-  const pageSelected = ref(false);
+  const pageSelected = computed(() => selectedPageMap.value.get(meta.value.current_page) === true);
+  const selectedPageMap = ref(new Map())
 
-  const actions = ref()
 
   const addSelectedItem = (item: Record<string, any>) => {
     selectedItems.value.push(item);
@@ -36,13 +41,25 @@ export const useGridStore = defineStore('grid', () => {
     return selectedItems.value.filter(i => i.id === item.id ).length;
   }
 
+  const deselectAll = () => {
+    selectedPageMap.value = new Map();
+    allSelected.value = false;
+    selectedItems.value = [];
+  }
+
   return {
     allSelected,
     pageSelected,
     selectedItems,
     selectedItemsLength,
+    selectedPageMap,
     setSelectedItems,
     selectItem,
-    isSelected
+    isSelected,
+    addSelectedItem,
+    removeSelectedItem,
+    init,
+    meta,
+    deselectAll
   }
 })

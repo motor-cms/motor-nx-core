c56c1374bbe62e27576bdfacee388a5184a18e04
Lukas Roth <mail@lukasroth.net>
2023-05-16 10:05:57 +0200

    added pusher

---
+++ store/notification.ts
@@ -0,0 +1,7 @@
+import {defineStore} from 'pinia'
+import Pusher from 'pusher-js';
+import * as PusherTypes from 'pusher-js';
+
+export const useNotificationStore = defineStore('notifications', () => {
+
+})

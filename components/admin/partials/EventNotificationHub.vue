9e3701139e141907908d88afb90a667381d6ac6a
Lukas Roth <mail@lukasroth.net>
2023-05-17 16:45:56 +0200

    styling

--- components/admin/partials/EventNotificationHub.vue
+++ components/admin/partials/EventNotificationHub.vue
@@ -1,14 +1,16 @@
 <template>
-  <fa icon="bell" @click="notificationCenterOpen = !notificationCenterOpen"></fa>
-  <Popover v-if="notificationCenterOpen">
-    <div class="alert alert-light alert-dismissible fade show" role="alert" v-for="notification in notifications">
-      <span class="alert-icon"><i class="ni ni-like-2"></i></span>
-      <span class="alert-text">{{ t(notification.message)}}</span>
-      <button type="button" class="btn-close" data-bs-dismiss="alert" @click="notificationStore.removeNotification(notification)" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-  </Popover>
+  <div class="position-relative">
+    <fa icon="bell" @click="notificationCenterOpen = !notificationCenterOpen" />
+    <Popover v-if="notificationCenterOpen">
+      <div class="alert alert-light alert-dismissible fade show m-0" role="alert" v-for="notification in notifications">
+        <span class="alert-icon"><i class="ni ni-like-2"></i></span>
+        <span class="alert-text">{{ t(notification.message)}}</span>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" @click="notificationStore.removeNotification(notification)" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </Popover>
+  </div>
 </template>
 <script setup lang="ts">
 import {useToast} from "vue-toastification";

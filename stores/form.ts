ccb19e225c2e6ac84b3eea684d21c6e215021ea3
Lukas Roth <mail@lukasroth.net>
2023-09-15 13:07:11 +0200

    added new file form

--- stores/form.ts
+++ stores/form.ts
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia'
+import {defineStore} from 'pinia'
 import {toTypedSchema} from "@vee-validate/yup";
 import * as yup from "yup";
 import {Ref} from "@vue/reactivity";
@@ -6,37 +6,51 @@ import {InferType} from "yup";
 
 export const useFormStore = defineStore('form', () => {
 
-    const model = ref<Record<string, any>>({});
-    const formData = ref<Record<string, any>>({});
-    const formSchema = ref({});
-    const form = ref({});
+  const model = ref<Record<string, any>>({});
+  const formData = ref<Record<string, any>>({});
+  const formSchema = ref({});
+  const form = ref({});
 
-    const schema = computed(() => toTypedSchema(yup.object({
-        ...formSchema.value
-    })))
+  /**
+   * Computed schema, if formSchema changes, the schema will be updated and schema in useForm (baseForm.ts) will be updated
+   */
+  const schema = computed(() => toTypedSchema(yup.object({
+    ...formSchema.value
+  })))
 
-    const init = (initialModelData: Record<string, any>, initialFormFata: Record<string, any>) => {
-        formData.value = JSON.parse(JSON.stringify(initialFormFata));
-        model.value = Object.assign({}, JSON.parse(JSON.stringify(initialModelData)), JSON.parse(JSON.stringify(initialFormFata)));
-    }
+  /**
+   * Initialize form with default values and the validation schema
+   * @param initialModelData
+   * @param initialFormFata
+   */
+  const init = (initialModelData: Record<string, any>, initialFormFata: Record<string, any>) => {
+    formData.value = JSON.parse(JSON.stringify(initialFormFata));
+    model.value = Object.assign({}, JSON.parse(JSON.stringify(initialModelData)), JSON.parse(JSON.stringify(initialFormFata)));
+  }
 
-    type ModelType = InferType<typeof schema>;
 
-    const fillModel = async (data: Partial<ModelType> | undefined | null) => {
-        model.value = data;
-    }
+  /**
+   * Fill model with data, only keys that are in model will be filled
+   * @param data
+   */
+  const fillModel = async (data: Record<string, any> | undefined | null) => {
+    model.value = Object.assign(...Object.keys(model.value).map(k => ({[k]: data[k]})));
+  }
 
-    watch(() => model.value, () => {
-        formData.value = Object.assign(...Object.keys(formData.value).map(k => ({[k] : model.value[k]})));
-    }, {deep: true})
+  /**
+   * Watch model and update formData
+   */
+  watch(() => model.value, () => {
+    formData.value = Object.assign(...Object.keys(formData.value).map(k => ({[k]: model.value[k]})));
+  }, {deep: true})
 
-    return {
-        model,
-        schema,
-        formSchema,
-        fillModel,
-        init,
-        formData,
-        form
-    }
+  return {
+    model,
+    schema,
+    formSchema,
+    fillModel,
+    init,
+    formData,
+    form
+  }
 })

41bf59e0b02a86fd0474a93424833e5f27599f73
Reza Esmaili <bitbucket@dfox.info>
2024-01-25 11:03:36 +0100

    Remove pusher

--- plugins/004.pusher.client.ts
+++ plugins/004.pusher.client.ts
@@ -6,16 +6,15 @@ import {watch} from "vue";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
 
-
-  const userStore = useUserStore();
-  const notificationStore = useNotificationStore();
-  const socket = useSocket();
-
-  watch(() => userStore.user, async () => {
-    await socket.initialize();
-    await socket.client.value.subscribe('private-user.' + userStore.user.id).bind(socket.events.export.created, async (data: Record<string, any>) => {
-      notificationStore.publish(data);
-    });
-  })
+  // const userStore = useUserStore();
+  // const notificationStore = useNotificationStore();
+  // const socket = useSocket();
+  //
+  // watch(() => userStore.user, async () => {
+  //   await socket.initialize();
+  //   await socket.client.value.subscribe('private-user.' + userStore.user.id).bind(socket.events.export.created, async (data: Record<string, any>) => {
+  //     notificationStore.publish(data);
+  //   });
+  // })
 
 })

5f83c8c1e3b306b59c61a1514375796a61af5edd
Jan Ehrlich <je@zeitraum.com>
2024-02-20 18:11:24 +0100

    Added Action Button for Funnels Table EC-544

---
+++ components/admin/cell/ActionButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="inline mb-0 ms-1">
+    <button
+      class="btn btn-primary btn-sm me-2"
+      type="button"
+      :title="options?.name"
+      @click="functionToCall()"
+      href="javascript:;"
+    >
+      {{ options?.name }}
+    </button>
+  </div>
+</template>
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const props = defineProps({
+  options: Object,
+});
+
+const { $toast } = useNuxtApp();
+const { t } = useI18n();
+
+const functionToCall = async () => {
+  if (props.options && props.options.onClick) {
+    props.options.onClick(props);
+  } else {
+    $toast.error(t("motor-nx-core::backend.no_on_click_defined"));
+    console.error("No onClick function defined for ActionButton");
+  }
+};
+</script>

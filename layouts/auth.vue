3a82882d22e426e65090c91a61894b256b60d389
Lukas Roth <mail@lukasroth.net>
2024-07-18 15:22:01 +0200

    added fancy layout switch

---
+++ layouts/auth.vue
@@ -0,0 +1,37 @@
+<template>
+    <AdminContainer>
+      <AdminPartialsSpinner v-if="loading"/>
+      <div class="builder-main-container">
+        <slot/>
+      </div>
+    </AdminContainer>
+</template>
+<script lang="ts" setup>
+
+import {storeToRefs} from "pinia";
+
+const appStore = useAppStore();
+const userStore = useUserStore();
+const {loading} = storeToRefs(appStore)
+</script>
+
+<style>
+.builder-main-container {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  .main-content {
+    //width: 100%;
+    margin-top: 50px;
+  }
+
+  .form-check-input:checked[type=checkbox] {
+    background-image: linear-gradient(310deg, var(--c-primary-100) 0%, var(--c-primary-80) 100%);
+  }
+
+  .atom-button {
+    pointer-events: none;
+  }
+}
+</style>
